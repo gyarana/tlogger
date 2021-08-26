c62cce16488e99826873cbfad91ca830c3a540dd
Yaroslav Kyshko <gyarana123@gmail.com>
2021-08-26 08:10:12 +0300

    Change functions outputs

---
+++ tlogger.go
@@ -0,0 +1,19 @@
+package tlogger
+
+import "fmt"
+
+func Info(message string, params interface{}) {
+  fmt.Println("Log info message: ", message, " params: ", params)
+}
+
+func Error(message string, params interface{}) {
+  fmt.Println("Log error message: ", message, " params: ", params)
+}
+
+func Warning(message string, params interface{}) {
+  fmt.Println("Log warning message: ", message, " params: ", params)
+}
+
+func Fatal(message string, params interface{}) {
+  fmt.Println("Log fatal: message", message, " params: ", params)
+}
