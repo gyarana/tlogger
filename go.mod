489b879af0427c20812f538b9cdb75100bde6b49
Yaroslav Kyshko <gyarana123@gmail.com>
2021-08-28 03:41:06 +0300

    Created new version

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/gyarana/tlogger/v3
+
+go 1.16
---
+++ tlogger.go
@@ -0,0 +1,64 @@
+package tlogger
+
+import (
+	"fmt"
+	"github.com/bmuller/arrow"
+	"github.com/gookit/color"
+	"log"
+	"os"
+	"time"
+)
+
+func infoFileLogger(msg string, data interface{}){
+	file:= getLogFile()
+	infoLog := log.New(file,"INFO:\t", log.Ldate|log.Ltime|log.Lshortfile)
+	infoLog.Println(msg,data)
+	file.Close()
+}
+
+func errorFileLogger(msg string, data interface{}){
+	file:= getLogFile()
+	errorLog := log.New(file,"ERROR:\t", log.Ldate|log.Ltime|log.Lshortfile)
+	errorLog.Println(msg,data)
+	file.Close()
+}
+
+func warningFileLogger(msg string, data interface{}){
+	file:= getLogFile()
+	warningLog := log.New(file,"WARNING:\t", log.Ldate|log.Ltime|log.Lshortfile)
+	warningLog.Println(msg,data)
+	file.Close()
+}
+
+func debugFileLogger(msg string, data interface{}){
+	file:= getLogFile()
+	debugLog := log.New(file,"DEBUG:\t", log.Ldate|log.Ltime|log.Lshortfile)
+	debugLog.Println(msg,data)
+	file.Close()
+}
+
+
+func infoLogger(msg string, data interface{}) {
+	color.Info.Println( "INFO: ", arrow.Now().CFormat("%H:%M:%S"), msg, data)
+}
+
+func debugLogger(msg string, data interface{}) {
+	color.Debug.Println( "DEBUG: ", arrow.Now().CFormat("%H:%M:%S"), msg, data)
+}
+
+func errorLogger(msg string, data interface{}) {
+	color.Error.Println( "ERROR: ", arrow.Now().CFormat("%H:%M:%S"), msg, data)
+}
+
+func warningLogger(msg string, data interface{}) {
+	color.Warn.Println( "WARNING: ", arrow.Now().CFormat("%H:%M:%S"), msg, data)
+}
+
+func getLogFile() *os.File{
+	fileName := fmt.Sprintf("logs/"+time.Now().Format("02_Jan_2006")+".txt")
+	file, err := os.OpenFile(fileName, os.O_CREATE|os.O_APPEND|os.O_RDWR, 0765)
+	if err!= nil{
+		return nil
+	}
+	return file
+}
